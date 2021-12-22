b091437cfb5c643da166f46160afe007b6e123b9
Emmanuel Thompson <ethompson@fastly.com>
2021-12-22 14:08:14 -0500

    Format options get re-written by ftplugins, try autocmd

--- config/nvim/plugin/options.lua
+++ config/nvim/plugin/options.lua
@@ -102,16 +102,23 @@ opt.smartcase = true
 -- incremental search off
 --opt.noincsearch
 
-opt.formatoptions = opt.formatoptions
-  - "a" -- Auto formatting is BAD.
-  - "t" -- Don't auto format my code. I got linters for that.
-  + "c" -- In general, I like it when comments respect textwidth
-  + "q" -- Allow formatting comments w/ gq
-  - "o" -- O and o, don't continue comments
-  + "r" -- But do continue when pressing enter.
-  + "n" -- Indent past the formatlistpat, not underneath it.
-  + "j" -- Auto-remove comments if possible.
-  - "2" -- I'm not in gradeschool anymore
+-- opt.formatoptions = opt.formatoptions
+--   - "a" -- Auto formatting is BAD.
+--   - "t" -- Don't auto format my code. I got linters for that.
+--   + "c" -- In general, I like it when comments respect textwidth
+--   + "q" -- Allow formatting comments w/ gq
+--   - "o" -- O and o, don't continue comments
+--   + "r" -- But do continue when pressing enter.
+--   + "n" -- Indent past the formatlistpat, not underneath it.
+--   + "j" -- Auto-remove comments if possible.
+--   - "2" -- I'm not in gradeschool anymore
+--
+vim.api.nvim_exec([[
+  augroup NoAutoComment
+    au!
+    au FileType * setlocal formatoptions-=ro
+  augroup end
+]], false)
 
 -- Disable backup files
 opt.backup = false
